c4785b011fd7dad28afbc43f54d5dde12bdeb81f
Joshua Szuslik <joshuaszuslik@gmail.com>
2016-09-22 20:32:54 -0500

    Update
    Update

---
+++ template-parts/content-guarantee.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-4 col-md-push-8 col-lg-4 col-lg-push-8">
+        <?php wpi_switch_img_url_to_data_string('/wp-content/uploads/2016/05/guarantee.jpg', 'full', '100% Satisfaction Guarantee'); ?>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-8 col-md-pull-4 col-lg-8 col-lg-pull-4">
+        <?php the_content(); ?>
+    </div>
+</div>
